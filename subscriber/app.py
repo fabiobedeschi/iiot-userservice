884d8c51e4b73cc6439a4484522f4d44514b303f
Paolo7297 <ca.paolo97@gmail.com>
2021-06-11 17:55:51 +0200

    Fixes

--- subscriber/app.py
+++ subscriber/app.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     subscriber = Subscriber()
     subscriber.connect(
         host=getenv('BROKER_HOST', 'mosquitto'),
-        port=int(getenv('BROKER_PORT', 1884)),
+        port=int(getenv('BROKER_PORT', 1884))
     )
     subscriber.loop_forever()
